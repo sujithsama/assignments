70d2fbc262d96b93739ae6755bfc654d2c5fd1ef
sujithsama <sujithsama2@gmail.com>
2023-02-19 15:49:56 +0530

    first commit

---
+++ New folder/script.js
@@ -0,0 +1,16 @@
+console.log('hello');
+var input=prompt("enter your age",18);
+if(input<18)
+{
+    console.log("you are minor");
+    alert("you are minor");
+}
+else{
+    
+        console.log("you are major");
+        alert("you are major");
+    
+}
+//alert("hi " +input);
+ let div=document.createAttribute("div");
+ div.innerText="helo";
